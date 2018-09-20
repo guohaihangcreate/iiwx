72469a506e37b172abefce2fd977eb9c227d7296
guohaihangcreate <guohaihang@xiangmu.ren>
2018-09-20 15:48:49 +0800

    添加过滤的时候检测演示有  .标识是资源文件请求路径，不如.css,.js,.jpg.......，此类请求不进行处理！

--- src/main/java/ren/xiangmu/iiwx/filter/SessionFilter.java
+++ src/main/java/ren/xiangmu/iiwx/filter/SessionFilter.java
@@ -18,21 +18,26 @@ public class SessionFilter implements Filter {
 	String NO_LOGIN = "您还未登录";
 
 	// 不需要登录就可以访问的路径(比如:注册登录等)
-	String[] includeUrls = new String[] { "/login_wx","/ajaxLogIn","/resetpassword","/wxAuthLogIn", "/ajaxRegistered", "/callback","/CodeServlet","/checkResetLink" };
+	String[] includeUrls = new String[] { "/login_wx", "/ajaxLogIn", "/resetpassword", "/wxAuthLogIn",
+			"/ajaxRegistered", "/callback", "/CodeServlet", "/checkResetLink", "/css/weui.css", "/css/example.css",
+			"/images/icon_tabbar.png","/js/example.js","/js/zepto.min.js","/js/jweixin-1.0.0.js"};
 
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		HttpSession session = request.getSession(false);
+		HttpSession session = request.getSession();
 		String uri = request.getRequestURI();
 
 		System.out.println("filter url:" + uri);
 		// 是否需要过滤
 		boolean needFilter = isNeedFilter(uri);
-
-		if (!needFilter) { // 不需要过滤直接传给下一个过滤器
+		//如果url中有点存在放行，标识是请求资源文件例如 .css,.image,.jpg,.js等等
+		if(uri.indexOf(".")>0) {
+			filterChain.doFilter(request, response);
+		}
+		else if (!needFilter) { // 不需要过滤直接传给下一个过滤器
 			filterChain.doFilter(servletRequest, servletResponse);
 		} else { // 需要过滤器
 			// session中包含user对象,则是登录状态
@@ -46,8 +51,8 @@ public class SessionFilter implements Filter {
 					response.getWriter().write(this.NO_LOGIN);
 				} else {
 					// 重定向到登录页(需要在static文件夹下建立此html文件)
-//					response.sendRedirect(request.getContextPath() + "/login_wx");
-					response.sendRedirect("/login_wx");
+					// response.sendRedirect(request.getContextPath() + "/login_wx");
+						response.sendRedirect("/login_wx");
 				}
 				return;
 			}
